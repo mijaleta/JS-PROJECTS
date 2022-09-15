02e95757e564e1eb91957e065dc9185525a9ae30
mijaleta <miratuujaallataa@gmail.com>
2022-09-15 14:15:21 +0300

    n

--- form validation.js
+++ form validation.js
@@ -1,12 +1,28 @@
-const errorElement = document.getElementById("error");   
-const form=document.getElementById("form");
+const name = document.getElementById('name')
 const password = document.getElementById('password')
-form.addEventListener('submit',(e)=>{
-    
-    let messages=[];
-    e.preventDefault();
-if(password.value.length >= 2){
-    messages.push('password must be less than 2');
-}
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
 
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
 })
