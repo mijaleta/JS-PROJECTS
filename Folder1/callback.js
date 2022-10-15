2f8356c3083c53d050ce965591d6b5a252f6dd4b
mijaleta <miratuujaallataa@gmail.com>
2022-10-15 18:08:32 +0300

    Update callback.js

--- Folder1/callback.js
+++ Folder1/callback.js
@@ -1,9 +1,14 @@
+//Well if you want to know more about call back you are well come 
+//if you want vedio link where i have refer this===https://www.youtube.com/watch?v=GWq0XETTOTk
+
 function makaUpperCase(value){
     console.log(value.toUpperCase())
     }
-   // makaUpperCase('my name is miretu jaleta chala futasa kasa miju ode shumakar')
 function handleName(name,cb){
     const fullName = `${name} Jaleta`
     cb(fullName)
 }
-handleName('mire',makaUpperCase)
+handleName('miretu',makaUpperCase)
+handleName('Takkalegn',(value)=>{
+    console.log(value.toUpperCase())
+});
